64ff92a2a59735cb4b3c903c885e1dfff8476ea2
Gawaka <paskm001@gmail.com>
2026-04-27 21:40:38 +0300

    home_work_lesson_3

---
+++ home_work_lesson_2/script.js
@@ -0,0 +1,34 @@
+
+// home_work_lesson_3_first_task
+
+const booleanValue = true;
+const stringValue = 'Hello!';
+const numberValue = 1;
+const nullValue = null;
+const undefinedValue = undefined;
+const objectValue = { name: 'John', age: 30 };
+const symbolValue = Symbol('unique');
+const bigIntValue = 9007199254740991n;
+
+console.log('Boolean Value:', typeof booleanValue);
+console.log('String Value:',typeof stringValue);
+console.log('Number Value:',typeof numberValue);
+console.log('Null Value:',typeof nullValue);
+console.log('Undefined Value:',typeof undefinedValue);
+console.log('Object Value:',typeof objectValue);
+console.log('Symbol Value:',typeof symbolValue);
+console.log('Bigint Value:',typeof bigIntValue);
+
+// home_work_lesson_3_second_task
+
+// const firstNumber = prompt('Enter the first number:');
+// const secondNumber = prompt('Enter the second number:');
+// const thirdNumber = prompt('Enter the third number:');
+
+// console.log(`Users numbers: ${firstNumber}, ${secondNumber}, ${thirdNumber}`);
+
+// home_work_lesson_3_third_task
+
+const number = '10369';
+
+console.log(number.split('').join(' '));
