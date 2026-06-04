39a62d847b836b2697df28623bf1fab80f4a8787
Gawaka <paskm001@gmail.com>
2026-06-04 22:26:17 +0300

    home_work_lesson_12.3

--- home_work_lesson_12/script03.js
+++ home_work_lesson_12/script03.js
@@ -1,33 +1,31 @@
 
-function createPythagorasTable(size) {
-    const container = document.querySelector('.wrapper');
-    const table = document.createElement('table');
-    table.className = 'pythagoras-table';
+const todo = document.querySelector('.todo');
+const input = document.querySelector('input');
+const btn = document.getElementById('1');
 
-    for (let i = 0; i <= size; i++) {
-        const row = document.createElement('tr');
-        
-        for (let j = 0; j <= size; j++) {
-            const cell = i === 0 || j === 0 ? document.createElement('th') : document.createElement('td');
+function createToDoList() {
+    btn.addEventListener('click', (e)=> {
+        if (input.value) {
+            const task = document.createElement('li');
+            task.classList.add('task');
+
+            const taskText = document.createElement('p');
+            const removeTask = document.createElement('button');
+            removeTask.classList.add('remove');
             
-            if (i === 0 && j === 0) {
-                cell.textContent = '';
-            } else if (i === 0) {
-                cell.textContent = j;
-            } else if (j === 0) {
-                cell.textContent = i;
-                cell.className = 'header-col';
-            } else {
-                cell.textContent = i * j;
-            }
+            removeTask.textContent += '❌';
+            taskText.innerHTML += input.value;
 
-            row.appendChild(cell);
-        }
-        table.appendChild(row);
-    }
-    
-    container.innerHTML = '';
-    container.appendChild(table);
+            input.value = '';
+
+            task.append(taskText, removeTask);
+            todo.append(task);
+
+            removeTask.addEventListener('click', ()=> {
+                task.remove();
+            });
+        };
+    });
 };
 
-createPythagorasTable(10);
+createToDoList();
