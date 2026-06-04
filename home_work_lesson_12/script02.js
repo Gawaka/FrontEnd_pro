f63f58683d7a2257153b5db7daeb48b23491480a
Gawaka <paskm001@gmail.com>
2026-06-04 22:34:50 +0300

    home_work_lesson_12.2 resubmission

--- home_work_lesson_12/script02.js
+++ home_work_lesson_12/script02.js
@@ -1,15 +1,9 @@
 const box = document.querySelector('.btn-box');
-const btn1 = document.getElementById('1');
-const btn2 = document.getElementById('2');
-const btn3 = document.getElementById('3');
 
 box.addEventListener('click', (e)=> {
     const target = e.target;
-    if(target === btn1) {
-        alert('Button: 1');
-    } else if(target === btn2) {
-        alert('Button: 2');
-    } else if (target === btn3) {
-        alert('Button: 3');
+
+    if (target.tagName === 'BUTTON') {
+        alert(`Button: ${target.textContent}`);
     }
 });
